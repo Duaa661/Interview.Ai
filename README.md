5a63964ef1b6b80759f0922b4fee6cbd5752cdf4
Dushyant Rajput <dushyantr737@gmail.com>
2026-06-18 13:08:23 +0530

    push code

---
+++ README.md
@@ -0,0 +1,2 @@
+# InterviewIQ-AGENT
+AI Interview Agent is a full-stack web application that conducts AI-powered mock interviews, generates role-specific questions, evaluates responses, provides personalized feedback, tracks interview history, and helps users improve their interview skills through an interactive and realistic interview experience.
---
+++ client/src/utils/firebase.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "iqinterview-43be8.firebaseapp.com",
+  projectId: "iqinterview-43be8",
+  storageBucket: "iqinterview-43be8.firebasestorage.app",
+  messagingSenderId: "199057417186",
+  appId: "1:199057417186:web:9ebb518370f31e03ec8fbb"
+};
+console.log(import.meta.env.VITE_FIREBASE_APIKEY)
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
