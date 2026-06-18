e6204065bf15bbcec5c05fd39b2053d8e6eb9bf2
Dushyant Chauhan <dushyantr737@gmail.com>
2026-06-18 13:37:48 +0530

    Update firebase.js

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,22 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "ai-website-builder-5bce0.firebaseapp.com",
-  projectId: "ai-website-builder-5bce0",
-  storageBucket: "ai-website-builder-5bce0.appspot.com",
-  messagingSenderId: "382935150780",
-  appId: "1:382935150780:web:6ebf77bd6b1a3a1c42a0b9",
+  authDomain: "iqinterview-43be8.firebaseapp.com",
+  projectId: "iqinterview-43be8",
+  storageBucket: "iqinterview-43be8.firebasestorage.app",
+  messagingSenderId: "199057417186",
+  appId: "1:199057417186:web:9ebb518370f31e03ec8fbb"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-  prompt: "select_account",
-});
+const provider = new GoogleAuthProvider()
 
-export { auth, provider };
+export {auth , provider}
